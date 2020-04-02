d20483f944dac6f3e8e87a6631222a2acbde4a01
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-02 09:02:14 -0400

    basic styling

--- src/components/controller/index.jsx
+++ src/components/controller/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import './styles.css'
 
 export default class Controller extends React.Component {
 
   render() {
     return (
       <div className="Controller">
-        <h1 className="title">JS-808</h1>
-        <button>stop</button>
-        <button>play</button>
-        <span>128</span>
-        {/* <input value="128"/> */}
-        <span>BPM</span>
-        <span>Sequence 1</span>
+        <div className="header">
+          <h1 className="title">JS-808</h1>
+          <span className="toolbar">
+            <button>stop</button>
+            <button>play</button>
+            <span>128</span>
+            {/* <input value="128"/> */}
+            <span>BPM</span>
+            <span>Sequence 1</span>
+          </span>
+        </div>
       </div>
     )
   }
