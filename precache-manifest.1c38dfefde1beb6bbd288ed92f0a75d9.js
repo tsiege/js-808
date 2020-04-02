12d4c9834d80dbc4233492f2ca4e370d79ff7e40
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-02 15:12:29 -0400

    Updates

---
+++ precache-manifest.1c38dfefde1beb6bbd288ed92f0a75d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281977775108654580db1a14dc500059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e261b3fef5368aafd7",
+    "url": "/static/css/main.aa3a5477.chunk.css"
+  },
+  {
+    "revision": "65e318821cc6b551bb89",
+    "url": "/static/js/2.672e3689.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.672e3689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e261b3fef5368aafd7",
+    "url": "/static/js/main.39e59102.chunk.js"
+  },
+  {
+    "revision": "59b743968c576b8241d1",
+    "url": "/static/js/runtime-main.007bfce4.js"
+  }
+]);
