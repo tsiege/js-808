5ff4b91e2e930b99a7955f5c8d50644416993812
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-02 09:24:46 -0400

    gets play button working

--- src/components/controller/index.jsx
+++ src/components/controller/index.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 import './styles.css'
 
 export default class Controller extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bpm: 128,
+      step: null,
+      interval: null,
+      isPlaying: false,
+    }
+  }
+
+  togglePlay = () => {
+    const { isPlaying, interval, bpm } = this.state
+    if (isPlaying) {
+      clearInterval(interval)
+      this.setState({ isPlaying: false, interval: null })
+    } else {
+      const interval = setInterval(() => {
+        const { step } = this.state
+        if (step === null || step === 15) {
+          this.setState({ step: 0 })
+        } else {
+          this.setState({ step: step + 1 })
+        }
+      }, bpm)
+      this.setState({ isPlaying: true, interval })
+    }
+  }
 
   renderSteps() {
     const steps = []
@@ -18,7 +45,7 @@ export default class Controller extends React.Component {
           <h1 className="title">JS-808</h1>
           <span className="toolbar">
             <button>stop</button>
-            <button>play</button>
+            <button onClick={this.togglePlay}>play</button>
             <span>128</span>
             {/* <input value="128"/> */}
             <span>BPM</span>
