fe853946f0dd980d83ea3a74346854993fd0cff2
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-02 08:47:35 -0400

    initial commit

---
+++ src/components/controller/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Controller extends React.Component {
+
+  render() {
+    return (
+      <div className="Controller">
+        <h1 className="title">JS-808</h1>
+        <button>stop</button>
+        <button>play</button>
+        <input value="128"/>
+        <span>BPM</span>
+        <span>Sequence 1</span>
+      </div>
+    )
+  }
+}
