34a839ce5d08e3ca5166cbca5c110b9f4a4c7f4d
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-04-02 10:32:38 -0400

    styles buttons for on/off

--- src/components/stepButton/index.jsx
+++ src/components/stepButton/index.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import './styles.css'
 
 export default class Track extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { on: '' }
+  }
+
+  toggleOn = () => {
+    const { on } = this.state
+    this.setState({ on: on ? '' : 'on' })
+  }
 
   render() {
-    return <button className={`step-button ${this.props.playing}`}></button>
+    return <button className={`step-button ${this.props.playing} ${this.state.on}`} onClick={this.toggleOn}></button>
   }
 }
